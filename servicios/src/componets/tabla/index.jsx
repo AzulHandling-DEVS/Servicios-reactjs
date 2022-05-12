4fec1510aa09816a5ef80d6793d7135b59932fac
lopem <manugrande@gmail.com>
2022-05-12 17:50:09 +0200

    Diseño acabado de servicios, farta meter lógica

--- servicios/src/componets/tabla/index.jsx
+++ servicios/src/componets/tabla/index.jsx
@@ -9,78 +9,86 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 
 const columns = [
-    { id: 'id', label: 'id', minWidth: 70 },
-    { id: 'date', label: 'Date', minWidth: 70 },
+    { id: 'id', label: 'id', width: '10px' },
+    { id: 'date', label: 'Date',width: '10px' },
     {
         id: 'service',
         label: 'Service',
-        minWidth: 70,
+        width: '10px',
         format: (value) => value.toLocaleString('en-US'),
     },
     {
         id: 'FlNoIn',
         label: 'Flight Nº In',
-        minWidth: 70,
+        width: '10px',
         format: (value) => value.toLocaleString('en-US'),
     },
     {
         id: 'FlNoOut',
         label: 'Flight Nº Out',
-        minWidth: 70,
+        width: '10px',
         format: (value) => value.toFixed(2),
     },
     {
         id: 'reg',
         label: 'REG',
-        minWidth: 70,
+        width: '10px',
         format: (value) => value.toFixed(2),
     },
     {
         id: 'gpunumber',
         label: 'GPU Number',
-        minWidth: 70,
+        width: '10px',
         format: (value) => value.toFixed(2),
     },
     {
         id: 'hoursnight',
         label: 'Hours Night',
-        minWidth: 70,
+        width: '10px',
         format: (value) => value.toFixed(2),
     },
     {
         id: 'hoursmorning',
         label: 'Hours morning',
-        minWidth: 70,
+        width: '10px',
         format: (value) => value.toFixed(2),
     },
     {
         id: 'comments',
         label: 'Comments',
-        minWidth: 70,
+        width: '10px',
         format: (value) => value.toFixed(2),
     },
 ];
 
-function createData(id, date, service, FlNoIn, FlNoOut, reg, gpunumber, hoursnight, hoursmorning) {
-    return { id, date, service, FlNoIn, FlNoOut, reg, gpunumber, hoursnight, hoursmorning };
+function createData(id, date, service, FlNoIn, FlNoOut, reg, gpunumber, hoursnight, hoursmorning, comments) {
+    return { id, date, service, FlNoIn, FlNoOut, reg, gpunumber, hoursnight, hoursmorning, comments };
 }
 
 const rows = [
-    createData('India', 'IN', 1324171354, 3287263),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
+    createData('d1234', '23/07/22', 'Buses', 9034, 9045, 'EINE', 'GPU-46', 0.8 , 0.2, 'First Wave'),
 ];
 
 export default function DataTable() {
@@ -105,7 +113,9 @@ export default function DataTable() {
                             {columns.map((column) => (
                                 <TableCell
                                     key={column.id}
-                                    style={{ minWidth: column.minWidth }}
+                                    style={{ minWidth: column.minWidth,
+                                    backgroundColor: '#06358F',
+                                    color: '#ffffff', }}
                                 >
                                     {column.label}
                                 </TableCell>
@@ -135,7 +145,7 @@ export default function DataTable() {
                 </Table>
             </TableContainer>
             <TablePagination
-                rowsPerPageOptions={[20, 100]}
+                rowsPerPageOptions={[10, 20, 100]}
                 component="div"
                 count={rows.length}
                 rowsPerPage={rowsPerPage}
